a429f0ba67bef9efb5545e93ddd032123d825d9b
dotku <weijingjaylin@gmail.com>
2021-05-23 17:23:28 -0700

    multipleline with autofocus

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,14 +1,34 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import * as React from "react";
+import { StyleSheet } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
 
 export default function TabOneScreen() {
+  const inputElement = React.useRef(null);
+  const [textInputHeight, setTextInputHeight] = React.useState(0);
+  React.useEffect(() => {
+    inputElement.current && inputElement.current.focus();
+  }, []);
   return (
     <View style={styles.container}>
+      <TextInput
+        multiline
+        autoFocus={true}
+        placeholder={"TextInput"}
+        ref={inputElement}
+        onContentSizeChange={(e) => {
+          setTextInputHeight(e.nativeEvent.contentSize.height);
+        }}
+        style={{ outline: "none", height: Math.max(35, textInputHeight) }}
+      />
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
@@ -17,16 +37,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
